47fee28fb50d156a9e8ce5bdc3f3e87966b13fc5
onozaty <onozaty@gmail.com>
2018-01-14 13:03:54 +0900

    WebSocketを使ったカメラ画像共有を実装(1台のカメラのみに対応)

---
+++ shared-camera-server/src/main/java/com/enjoyxstudy/sharedcamera/SpringBootSharedCameraApplication.java
@@ -0,0 +1,12 @@
+package com.enjoyxstudy.sharedcamera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSharedCameraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSharedCameraApplication.class, args);
+    }
+}
---
+++ shared-camera-server/src/main/resources/static/shared-camera.js
@@ -0,0 +1,111 @@
+class SharedCamera {
+  constructor(endpoint, handlers) {
+    this._endpoint = endpoint;
+    this._handlers = handlers;
+  }
+
+  connect() {
+    this._webSocket = new WebSocket(this._endpoint);
+
+    this._webSocket.onopen = (event) => {
+      this._onOpen();
+    };
+
+    this._webSocket.onclose = (event) => {
+      this._onClose(event);
+    };
+
+    this._webSocket.onmessage = (event) => {
+      this._onMessage(event);
+    };
+
+    this._webSocket.onerror = (event) => {
+      this._onError(event);
+    };
+  }
+
+  disconnect() {
+    this._webSocket.close();
+  }
+
+  enableCamera(video, interval, failureCallback) {
+    navigator.mediaDevices.getUserMedia({audio: false, video: true})
+      .then((stream) => {
+        video.srcObject = stream;
+        video.autoplay = true;
+        this._startShooting(video, interval); 
+        this._video = video;
+      })
+      .catch((error) => {
+        failureCallback(error);
+      });
+  }
+
+  disableCamera() {
+  
+    this._video.srcObject.getTracks().forEach((track) => {
+      track.stop();
+    });
+  
+    this._video.srcObject = null;
+    this._video = null;
+
+    if (this._timerId) {
+      clearTimeout(this._timerId);
+      this._timerId = null;
+    }
+  }
+
+  _startShooting(video, interval) {
+    if (this._timerId) {
+      clearTimeout(this._timerId);
+    }
+
+    this._timerId = setInterval(() => {
+      this._send(video);
+    }, interval);
+  }
+
+  _send(video) {
+    if (!this._webSocket) {
+      return;
+    }
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.setAttribute('width', video.videoWidth);
+    canvas.setAttribute('height', video.videoHeight);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    canvas.toBlob((blob) => {
+      if (blob && this._webSocket) {
+        this._webSocket.send(blob);
+      }
+    }, 'image/jpeg', 0.8);
+  }
+
+  _onOpen() {
+    if (this._handlers.open) {
+      this._handlers.open();
+    }
+  }
+
+  _onClose(event) {
+    this._webSocket = null;
+    if (this._handlers.close) {
+      this._handlers.close(event);
+    }
+  }
+
+  _onError(event) {
+    if (this._handlers.error) {
+      this._handlers.error(event);
+    }
+  }
+
+  _onMessage(event) {
+    if (this._handlers.message) {
+      this._handlers.message(event);
+    }
+  }
+}
