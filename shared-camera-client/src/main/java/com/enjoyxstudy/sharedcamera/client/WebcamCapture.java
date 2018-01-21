8d966015124fc4d63def8ae5292a021245c41f4a
onozaty <onozaty@gmail.com>
2018-01-22 01:02:58 +0900

    v4l4jを使えるよう変更。

--- shared-camera-client/src/main/java/com/enjoyxstudy/sharedcamera/client/WebcamCapture.java
+++ shared-camera-client/src/main/java/com/enjoyxstudy/sharedcamera/client/WebcamCapture.java
@@ -11,17 +11,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.ds.v4l4j.V4l4jDriver;
 
 @Component
 public class WebcamCapture {
 
-    @Value("${capture.interval-millis}")
+    @Value("${camera.interval-millis}")
     private int intervalMillis;
+
+    @Value("${camera.use-v4l4j}")
+    private boolean useV4l4j;
 
     @Autowired
     private ImageWebSocketHandler webSocketHandler;
 
     public void startCapture() throws IOException, InterruptedException {
+
+        if (useV4l4j) {
+            Webcam.setDriver(new V4l4jDriver());
+        }
 
         Webcam webcam = Webcam.getDefault();
 
